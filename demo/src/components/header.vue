1f847046b54b482fc04f4687e3b10c256d77fd2e
永赞 朱 <z_praise@outlook.com>
2017-07-28 09:55:14 +0800

    '项目'

---
+++ demo/src/components/header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>共同header</h1>
+        <img src="../assets/imgs/logo.png">
+    </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+       img: 'https://www.baidu.com/img/bd_logo1.png'
+    }
+  },
+  methods:{
+    
+  }
+}
+</script>
